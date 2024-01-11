38187bc5168425cb9a504a5a7df3baefa54de470
kalyanit1 <156274160+kalyanit1@users.noreply.github.com>
2024-01-11 21:40:08 +0530

    Update demo_git1.java

--- github_project/src/github_/demo_git1.java
+++ github_project/src/github_/demo_git1.java
@@ -5,7 +5,21 @@ public class demo_git1 {
 	public static void main(String[] args) {
 		
 		System.out.println(" this is demo git1");
-
+                System.out.println("this is testing")
+	}
+public void m7()
+	{
+		System.out.println("this is m7 method")
+	}
+public void m8()
+	{
+		System.out.println("this is m8 method")
 	}
 
+public void m9()
+	{
+		System.out.println("this is m9 method")
+	}
+
+	
 }
