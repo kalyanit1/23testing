8471b426452a4a952912e4901d90205f5ffd396f
Raju <Raju@LAPTOP-Q4CHNOA3>
2024-01-12 10:27:25 +0530

    by kalyani1

---
+++ github_project/src/github_/demo_A.java
@@ -0,0 +1,10 @@
+package github_;
+
+public class demo_A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- github_project/src/github_/demo_git1.java
+++ github_project/src/github_/demo_git1.java
@@ -5,24 +5,24 @@ public class demo_git1 {
 	public static void main(String[] args) {
 		
 		System.out.println(" this is demo git1");
-                System.out.println("this is testing")
+                System.out.println("this is testing");
 	}
 public void m7()
 	{
-		System.out.println("this is m7 method")
+		System.out.println("this is m7 method");
 	}
 public void m8()
 	{
-		System.out.println("this is m8 method")
+		System.out.println("this is m8 method");
 	}
 
 public void m9()
 	{
-		System.out.println("this is m9 method")
+		System.out.println("this is m9 method");
 	}
 
 	public void m10()
 	{
-		System.out.println("this is m10 method")
+		System.out.println("this is m10 method");
 	}
 }
